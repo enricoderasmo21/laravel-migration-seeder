f89a8405a281136e417fd914b6faf00debee2b1b
enricoderasmo21 <derasmo.enrico@gmail.com>
2023-05-11 14:59:33 +0200

    created new 'trains' table

---
+++ database/migrations/2023_05_11_124609_create_trains_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('company');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->time('departure_hour');
+            $table->time('arrival_hour');
+            $table->string('train_code');
+            $table->tinyInteger('carriages_number');
+            $table->boolean('is_on_time');
+            $table->boolean('is_deleted');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
